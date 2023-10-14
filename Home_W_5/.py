c67f28036add485b3aa8a85792c5ce40c635f150
Ivan <ragna.blood82@gmail.com>
2023-10-14 16:32:11 +0300

    Nision two

--- Home_W_5/.py
+++ Home_W_5/.py
@@ -1,5 +1,17 @@
+
+                                                           #Задание первое
 num_zeroes = 0
 for i in range(int(input())):
     if int(input()) == 0:
         num_zeroes += 1
 print(num_zeroes)
+
+                                                            #Задание второе
+
+x = int(input())
+count = 0
+for i in range(1, x+1):
+    if x % i == 0:
+        count += 1
+
+print(count)
