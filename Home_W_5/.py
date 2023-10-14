67e61a641beb99673cf2e7842bb9dd1cf180ed28
Ivan <ragna.blood82@gmail.com>
2023-10-14 17:00:19 +0300

    Process

--- Home_W_5/.py
+++ Home_W_5/.py
@@ -1,10 +1,18 @@
 
                                                            #Задание первое
-num_zeroes = 0
-for i in range(int(input())):
-    if int(input()) == 0:
-        num_zeroes += 1
-print(num_zeroes)
+n=int(input("Введите количество чисел "))
+
+a=0
+
+for i in range(1, n+1):
+
+ s = int(input("введите целое число "))
+
+if s==0:
+
+ a+=1
+
+print(a)
 
                                                             #Задание второе
 
@@ -15,3 +23,18 @@ for i in range(1, x+1):
         count += 1
 
 print(count)
+
+                                                          # Задание третье
+print("Введите целое число А: ")
+
+a=int(input())
+
+print("Введите число B: ")
+
+b = int(input())
+
+for i in range(a, b + 1):
+
+ if i % 2 == 0:
+
+  print(i, end=' ')                                                    
